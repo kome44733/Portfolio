dca56f54db62e229f37eb57a2a206c49b57472ac
kome44733 <110373715+kome44733@users.noreply.github.com>
2022-10-22 06:40:36 +0000

    profession_show[edit]

--- app/controllers/public/professions_controller.rb
+++ app/controllers/public/professions_controller.rb
@@ -1,14 +1,14 @@
 class Public::ProfessionsController < ApplicationController
-  
+
   def index
     @professions = Profession.all
-    
+
   end
-  
+
   def show
-    profession = Profession.find(params[:id])
-    @best_answers = profession.questions.where(best_answer_id: nil).order(created_at: :desc).page(params[:page])
-    @no_best_answers = profession.questions.where.not(best_answer_id: nil).order(created_at: :desc).page(params[:page])
+    @profession = Profession.find_by(id: params[:id])
+    @best_answers = @profession.questions.where(best_answer_id: nil).order(created_at: :desc).page(params[:page])
+    @no_best_answers = @profession.questions.where.not(best_answer_id: nil).order(created_at: :desc).page(params[:page])
   end
-  
+
 end
